cc21f4321c96a1127cf7bbc3ae6d69b110833947
webdev817 <developer@jorgeolaya.co>
2020-11-04 05:24:00 -0400

    side_bar

--- src/components/ProfileTab.js
+++ src/components/ProfileTab.js
@@ -5,6 +5,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Divider from '@material-ui/core/Divider';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -42,9 +43,12 @@ function a11yProps(index) {
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
     display: 'flex',
-    height: 224,
+    justifyContent: 'space-around',
+    marginLeft: '15rem',
+    marginRight: '15rem',
+    marginTop: '5rem',
+    height: '65%'
   },
   tabs: {
     borderRight: `1px solid ${theme.palette.divider}`,
@@ -63,38 +67,41 @@ export default function ProfileTabs() {
     <div className={classes.root}>
       <Tabs
         orientation="vertical"
+        variant="scrollable"
         value={value}
         onChange={handleChange}
         aria-label="Vertical tabs example"
-        className={classes.tabs}
+        className='tabs'
       >
         <Tab className='tab-cell' label="Profile" {...a11yProps(0)} />
+        <Divider />
         <Tab className='tab-cell' label="Login & Security" {...a11yProps(1)} />
+        <Divider />
         <Tab className='tab-cell' label="Billing & Wallet" {...a11yProps(2)} />
+        <Divider />
         <Tab className='tab-cell' label="Privacy" {...a11yProps(3)} />
+        <Divider />
         <Tab className='tab-cell' label="Notifications" {...a11yProps(4)} />
+        <Divider />
         <Tab className='tab-cell' label="Recently Viewed" {...a11yProps(5)} />
       </Tabs>
-      <TabPanel value={value} index={0}>
-        User Info
+      <TabPanel className='table-panel' value={value} index={0}>
+        Item One
       </TabPanel>
-      <TabPanel value={value} index={1}>
+      <TabPanel className='table-panel' value={value} index={2}>
         Item Two
       </TabPanel>
-      <TabPanel value={value} index={2}>
+      <TabPanel className='table-panel' value={value} index={4}>
         Item Three
       </TabPanel>
-      <TabPanel value={value} index={3}>
+      <TabPanel className='table-panel' value={value} index={6}>
         Item Four
       </TabPanel>
-      <TabPanel value={value} index={4}>
+      <TabPanel className='table-panel' value={value} index={8}>
         Item Five
       </TabPanel>
-      <TabPanel value={value} index={5}>
+      <TabPanel className='table-panel' value={value} index={10}>
         Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
       </TabPanel>
     </div>
   );
